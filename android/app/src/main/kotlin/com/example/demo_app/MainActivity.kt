2f2e9853592e2ae57f3f3cc9502f87276ff647c6
yogeshcloudwapp <yogeshbutani.cloudwapp@gmail.com>
2025-01-20 19:15:14 +0530

    20.Jan

--- android/app/src/main/kotlin/com/example/demo_app/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/demo_app/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.demo_app
+package com.test.app
 
 import io.flutter.embedding.android.FlutterActivity
 
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
+import 'package:demo_app/firebase_options.dart';
 import 'package:demo_app/utils/app_file_imports.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(
     /// Using provider state management
     AppProviders.getProviders(
